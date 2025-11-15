a9d7097770282f95d75d15374a1bc2b23eea7d75
bola-cloud <bola.ishak41@gmail.com>
2025-11-15 18:02:06 +0200

    update profile + add device

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(), // Set the language prefix correctly
-    'middleware' => [
-        'auth:sanctum',
-        config('jetstream.auth_session'),
-        'verified',
-    ]
-], function () {
-});
+// Route::group([
+//     'prefix' => LaravelLocalization::setLocale(), // Set the language prefix correctly
+//     'middleware' => [
+//         'auth:sanctum',
+//         config('jetstream.auth_session'),
+//         'verified',
+//     ]
+// ], function () {
+// });
     Route::get('/', [\App\Http\Controllers\Admin\Dashboard::class, 'index'])->name('dashboard');
